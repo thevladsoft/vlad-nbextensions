1d31ae8723b9063425511d21d40ea1b7311c579a
Ilich Vladimir Idler <vidler@fenrir.labd.usb.ve>
2015-05-25 15:05:32 -0430

    Comentarios tipo kate/kwrite

---
+++ kate-commenter.js
@@ -0,0 +1,19 @@
+//cargar con IPython.load_extensions('kate-commenter'); en custom.js
+$([IPython.events]).on("app_initialized.NotebookApp", function () {
+//window.alert("HELP!");
+/*IPython.CodeCell.options_default.cm_config.extraKeys= {
+                "Tab" :  "indentMore",
+                "Shift-Tab" : "indentLess",
+                "Backspace" : "delSpaceToPrevTabStop",
+                "Cmd-/" : "toggleComment",
+                "Ctrl-/" : "toggleComment",//Los siguientes son los mios
+                "Ctrl-D": "toggleComment",
+                "Shift-Ctrl-D": "toggleComment",
+                "Shift-Alt-D": "deleteLine"
+            } */
+//Forma más corta y probablemente más segura
+IPython.CodeCell.options_default.cm_config.extraKeys["Ctrl-D"] = "toggleComment";
+IPython.CodeCell.options_default.cm_config.extraKeys["Shift-Ctrl-D"] = "toggleComment";
+IPython.CodeCell.options_default.cm_config.extraKeys["Shift-Alt-D"] = "deleteLine";
+
+});
